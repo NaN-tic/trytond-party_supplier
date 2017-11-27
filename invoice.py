b60a9dc3808e95f4e9fdb2229f8ffb90fb3c83d7
Raimon Esteve <raimon@nan-tic.com>
2017-11-28 00:06:11 +0100

    Add supplier party domain in product supplier task-032809

---
+++ invoice.py
@@ -0,0 +1,21 @@
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.pool import PoolMeta
+from trytond.pyson import If, Eval
+
+__all__ = ['Invoice']
+
+
+class Invoice:
+    __metaclass__ = PoolMeta
+    __name__ = 'account.invoice'
+
+    @classmethod
+    def __setup__(cls):
+        super(Invoice, cls).__setup__()
+        supplier_domain = [If(Eval('type') == 'in',
+                ('supplier', '=', True),
+                (),
+                )]
+        cls.party.domain.append(supplier_domain)
+        cls.party.depends.append('type')
--- tests/test_party_supplier.py
+++ tests/test_party_supplier.py
@@ -1,8 +1,12 @@
+# This file is part of the party_supplier module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
+import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import (doctest_setup, doctest_teardown,
+    doctest_checker)
 
 
 class PartySupplierTestCase(ModuleTestCase):
@@ -13,5 +17,9 @@ class PartySupplierTestCase(ModuleTestCase):
 def suite():
     suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-            PartySupplierTestCase))
+        PartySupplierTestCase))
+    suite.addTests(doctest.DocFileSuite('scenario_party_supplier.rst',
+            setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
+            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE,
+            checker=doctest_checker))
     return suite
