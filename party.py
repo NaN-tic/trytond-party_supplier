07986436aa6a59be0c479cb76c7f9f5646cb23b6
Raimon Esteve <raimonesteve@gmail.com>
2020-04-29 11:40:14 +0200

    Set all fields readonly for inactive records issue3747

--- party.py
+++ party.py
@@ -2,11 +2,10 @@
 # the full copyright notices and license terms.
 from trytond.model import fields
 from trytond.pool import PoolMeta
-from trytond.modules.party.party import STATES, DEPENDS
 
 __all__ = ['Party']
 
 
 class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
-    supplier = fields.Boolean('Supplier', states=STATES, depends=DEPENDS)
+    supplier = fields.Boolean('Supplier')
