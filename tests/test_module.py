08263f2eccbb1d2f2ac5206c23319060c45c7ea4
Albert Cervera i Areny <albert@nan-tic.com>
2022-05-03 16:16:40 +0200

    Remove CompanyTestMixin

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,12 +1,9 @@
-
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
-
-from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartySupplierTestCase(CompanyTestMixin, ModuleTestCase):
+class PartySupplierTestCase(ModuleTestCase):
     'Test PartySupplier module'
     module = 'party_supplier'
 
